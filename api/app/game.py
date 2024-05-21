b32fe093c49dba3f9a63fdbbb86d55977653d353
m20034 <m20034@g.metro-cit.ac.jp>
2024-05-21 02:54:56 +0000

    [add] gameの初期設定した

---
+++ api/app/game.py
@@ -0,0 +1,10 @@
+from user import User
+from board import Board
+
+class Game:
+    def __init__(self, color):
+        user1 = User("w")
+        user2 = User("b")
+        board = Board()
+    
+    
