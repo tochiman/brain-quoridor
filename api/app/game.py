df8e84652b5addd3a67ad8a7bfa92eb6cd1a4833
k175jp <tai.sp.tai@gmail.com>
2024-08-01 10:52:46 +0000

    [add] 幅探査の実装

--- api/app/game.py
+++ api/app/game.py
@@ -1,4 +1,5 @@
 from user import User
+from copy import deepcopy
 
 class Game:
     def __init__(self):
@@ -11,7 +12,7 @@ class Game:
         self.users[uid]["user"].wall -= 1
 
 
-    def check_wall(self, x, y, wall_type, uid): #壁を置けるか確認(未実装)
+    def check_wall(self, x, y, wall_type, uid):
         if self.users[uid]["user"].wall == 0:
             return False
         if not (0 <= x <= 8 and 0 <= y <= 8):
@@ -27,8 +28,26 @@ class Game:
                     if board[0] == (x,y+1) or board[0] == (x,y-1):
                         return False
 
-    def BFS(self):
-        pass
+        return BFS(x, y, wall_type):
+
+    def BFS(self, x, y, wall_type):
+        b = deepcopy(self.board)
+        b.append([(x, y), wall_type])
+        result = [False, False]
+        for index, _user in enumerate(self.users.values()):
+            user = _user["user"]
+            color = user.color
+            l = deepcopy(user.move_list)
+            for i in l:
+                x, y = i
+                if user.reach_goal_do(y, color):
+                   result[index] = True
+                   break
+                tmp = user.make_move_list_do(x, y, b, ())
+                for t in tmp:
+                    if not t in l:
+                        l.append(t)
+        return (result[0] and result[1])
 
     def uid_link(self, color, uid, user_name):
         self.users[uid] = {"user_name": user_name, "user": User(color), "ws": None}
@@ -52,9 +71,8 @@ class Game:
                     "wall": user.wall,
                     "other_wall": other.wall,
                     "color": user.color,
-                    "movable": user.movable,
-                    "can_put_wall": user.can_put_wall,
-                    "board": self.board.board
+                    "move_list": user.move_list,
+                    "board": self.board
                     })
             else:
                 ws.send_json({
@@ -64,7 +82,7 @@ class Game:
                     "wall": user.wall,
                     "other_wall": other.wall,
                     "color": user.color,
-                    "board": self.board.board
+                    "board": self.board
                     })
 
 
--- api/app/user.py
+++ api/app/user.py
@@ -20,8 +20,12 @@ class User:
             raise UserException
 
     def reach_goal(self):
-        _,y = self.position
-        if self.coler == "w":
+        _, y = self.position
+        color = self.color
+        return self.reach_goal_do(y, color)
+
+    def reach_goal_do(self, y, color):
+        if coler == "w":
             if y == 0:
                 return True
         else:
@@ -33,34 +37,39 @@ class User:
         return (x,y) in self.move_list
 
     def make_move_list(self, board, other_position):
-        x,y = self.position
-        self.move_list = []
+        x, y = self.position
+        self.move_list = self.make_move_list_do(x, y, board, other_position)
+
+    def make_move_list_do(self, x, y, board, other_position):
+        move_list = []
         if (not [(x-1, y-1), "h"] in board) and (not [(x, y-1), "h"] in board) and (0 <= x <=8 and 0 <= y-1 <= 8): #up　壁がない
             if other_position == (x,y-1): #相手の駒がある
                 if (not [(x-1, y-2), "h"] in board) and (not [(x, y-2), "h"] in board) and (0 <= x <=8 and 0 <= y-2 <= 8): #壁がない
-                    self.move_list.append((x,y-2))
+                    move_list.append((x,y-2))
                 else: #壁がある
                     
             else:
-                self.move_list.append((x,y-1))
+                move_list.append((x,y-1))
 
         if (not [(x-1, y), "h"] in board) and (not [(x, y), "h"] in board) and (0 <= x <=8 and 0 <= y+1 <= 8): #down
             if other_position == (x,y+1):
                 pass
             else:
-                self.move_list.append((x,y+1))
+                move_list.append((x,y+1))
 
         if (not [(x-1, y-1), "v"] in board) and (not [(x-1, y), "v"] in board) and (0 <= x-1 <=8 and 0 <= y <= 8): #left
             if other_position == (x-1,y):
                 pass
             else:
-                self.move_list.append((x-1,y))
+                move_list.append((x-1,y))
 
         if (not [(x, y-1), "v"] in board) and (not [(x, y), "v"] in board) and (0 <= x+1 <=8 and 0 <= y <= 8): #right
             if other_position == (x+1,y):
                 pass
             else:
-                self.move_list.append((x+1,y))
+                move_list.append((x+1,y))
+
+        return move_list
 
 
     def move(self, x, y):
